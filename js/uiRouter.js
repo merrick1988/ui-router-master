c4cfb1877cffa17cbcfce40d379d49926aeb7677
merrick1988 <merrick198@gmail.com>
2013-09-18 16:24:57 +0300

    add hash parsing on first load

--- js/uiRouter.js
+++ js/uiRouter.js
@@ -1,53 +1,58 @@
 // Make sure to include the `ui.router` module as a dependency.
-angular.module('uiRouterSample', [])
-	.controller("mainController",
-	    ['$scope', 'uiRouter',
-	      function ($scope, uiRouter) {
-	      }]).
+angular.module('uiRouterSample', []).
 	service('uiRouter', ['$rootScope', function($rootScope){
-		var Router, location,
+		var self = this,
+			location = window.location,
 			init;
-		location = window.location;
 
-		Router = function () {
+		self.currentState = null;
 
-		};
-		Router.prototype.setState = function(state){
-			this.currentState = state;
+		self.setState = function(state){
+			self.currentState = state;
 			location.hash = state;
 			$rootScope.$emit("stateChanged", state);
 
-		}
-		Router.prototype.getState = function(){
-			return this.currentState;
+		};
+
+		self.getState = function(){
+			return self.currentState;
 		};
 
 		init = function(){
+			// parse url hash to set current state on page reloading
 			if(location.hash){
 				var hash = location.hash.slice(1);
-				this.currentState = hash ;
-				Router.prototype.setState(hash);
+				hash && self.setState(hash);
 			};
 		};
 
 		init();
-		return new Router()
 	}]).
 	directive('uiState', ['$rootScope', 'uiRouter', function($rootScope, uiRouter){
 		return {
 			link: function(scope, element, attr){
-				var uiState = attr['uiState'];
+				var uiState = attr['uiState'],
+					init;
+
+				init = function(){
+					if(!uiRouter.currentState){
+						uiRouter.setState(uiState);
+					};
+					if(uiRouter.currentState === uiState){
+						element.addClass("active")
+					};
+				};
 
 				element.bind("click", function(){
 					uiRouter.setState(uiState);
 				});
 
 				$rootScope.$on('stateChanged', function(event, state){
-					if(attr['uiMenu'] !== undefined){
-						element.removeClass("active");
-						state === uiState && element.addClass("active")
-					}
-				})
+					element.removeClass("active");
+					state === uiState && element.addClass("active");
+				});
+
+				init();
 			}
 		}
 	}]).
